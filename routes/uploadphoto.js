2d108b3178f3937bc8558310b23d99a56c18c244
clumsycoder <tosendanand@gmail.com>
2019-07-27 20:00:15 +0530

    routes added

--- routes/uploadphoto.js
+++ routes/uploadphoto.js
@@ -1,10 +1,47 @@
 // call all the required packages
 const express = require('express')
 const bodyParser= require('body-parser')
-const router = express.Router();
+const multer = require('multer');
+const mongoose = require('mongoose');
+const fs = require('fs');
 
-router.post('/',(req,res)=>{
-    
+const router = express.Router();
+mongoose.connect("mongodb://localhost/imgphotoecom",{ useNewUrlParser: true });
+
+
+//UPLOADING FILES TO THE "UPLOAD" FOLDER IN THE LOCAL FILE DIRECTORY
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now()+'-'+file.originalname)
+    }
 })
+
+var upload = multer({ storage: storage })
+
+// USING MONGODB DATABASE AS A SERVER TO STORE THE THA DATA AND RETREVING THE DATA WHEN THE TIME COMES
+router.post('/',upload.single('myImage'),(req,res)=>{
+    var img = fs.readFileSync(req.file.path);
+    var encode_image = img.toString('base64');
+    // console.log(req.file);
+    var finalImg = {
+        name: String,
+        image:new Buffer.from(encode_image,'base64')
+    };
+
+    const images=mongoose.model('image',finalImg)
+    var image=new images
+    // image.contentType=req.file.mimetype
+    image.image=encode_image
+    image.name=req.originalname
+    image.save((err,res)=>{
+        // console.log(err);
+    })
+    res.send('UpLOADED ')
+    console.log(res);
+})
+
 
 module.exports=router;
